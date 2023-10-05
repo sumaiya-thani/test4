b1a31d3425756baa9b6a3cb3a0af17a2649b052a
sumaiya-thani <sumaiyathani@gmail.com>
2023-10-05 12:16:05 +0300

    Initial Commit

---
+++ test4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test4
+//
+//  Created by sumaiya on 05/10/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test4/test4App.swift
@@ -0,0 +1,17 @@
+//
+//  test4App.swift
+//  test4
+//
+//  Created by sumaiya on 05/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct test4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
